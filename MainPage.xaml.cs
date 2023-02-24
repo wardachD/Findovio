b2f1f7096078e6f9c953f074d27cc26241c3808c
Damiks <Damiks@DESKTOP-PU5U0CN>
2023-02-24 18:51:43 +0100

    logo

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,14 @@
-﻿namespace Findovio;
+﻿using Microsoft.Maui.Controls;
+using Findovio.ViewModels;
 
-public partial class MainPage : ContentPage
+namespace Findovio
 {
-	int count = 0;
-
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+    }
 }
-
---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Controls;
+using System.Windows.Input;
+
+namespace Findovio.ViewModels
+{
+    public class MainPageViewModel : BindableObject
+    {
+        public ICommand ButtonClickedCommand { get; }
+
+        public MainPageViewModel()
+        {
+            ButtonClickedCommand = new Command(() =>
+            {
+                // Tutaj możesz dodać kod wykonywany po kliknięciu przycisku
+            });
+        }
+    }
+}
